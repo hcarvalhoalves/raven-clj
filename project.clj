353939d2ad3bf9fd31bd3af729b72f3c8f4d3a3d
Brandon Adams <emidln@gmail.com>
2015-10-09 09:46:19 -0500

    Upgrade deps

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://github.com/emidln/raven-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[cheshire "5.0.1"]
-                 [clj-http "0.6.4"]
-                 [org.clojure/clojure "1.5.1"]])
+  :dependencies [[cheshire "5.5.0"]
+                 [clj-http "2.0.0"]
+                 [org.clojure/clojure "1.7.0"]])
